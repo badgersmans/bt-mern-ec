923bd981741d4ddc9683e3a96ad5a86e26a068f7
badgersmans <s.hawnlaw6669@gmail.com>
2020-10-01 10:22:53 +0800

    c2/005 react-bootstrap, add Header and Footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
 import React, { Fragment } from 'react';
+import { Container } from 'react-bootstrap';
 import './App.css';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
-const App = () => {
+const App = () => { 
   return (
     <Fragment>
-      <h1>e commerce</h1>
+
+      <Header />
+
+      <main className="py-4">
+        <Container>
+          <h1>Welcome to Geminids</h1>
+        </Container>
+      </main>
+
+      <Footer />
+
     </Fragment>
   );
 };
