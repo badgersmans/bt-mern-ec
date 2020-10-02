591741c6514c8f3ac70a958cd7969bc33330b803
badgersmans <s.hawnlaw6669@gmail.com>
2020-10-02 16:06:05 +0800

    c4/022 fetching products from database

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,8 @@ import express  from 'express';
 import dotenv   from 'dotenv';
 import colors   from 'colors';
 import connectDB from './config/db.js';
-import products from './data/products.js';
+
+import productRoutes from './routes/productRoutes.js';
 
 dotenv.config();
 
@@ -11,21 +12,15 @@ const app = express();
 // connect DB
 connectDB();
 
+
 // middlewares
 app.use(express.json({ extended: false }));
 
-const PORT = process.env.PORT || 4000;
 
 // define routes
-app.get('/api/products', (req, res) => {
-    res.json(products)
-});
+ app.use('/api/products', productRoutes);
 
-app.get('/api/products/:id', (req, res) => {
 
-    const product = products.find(p => p._id === req.params.id);
-
-    res.json(product)
-});
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => { console.log(`${process.env.NODE_ENV} server started on port ${PORT}`.underline.bold.blue); });
