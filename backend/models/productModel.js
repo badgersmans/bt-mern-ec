757c027d1492d44490c0e752e6229438e1f8df8f
badgersmans <s.hawnlaw6669@gmail.com>
2020-10-02 13:37:50 +0800

    c4/019 data models

---
+++ backend/models/productModel.js
@@ -0,0 +1,79 @@
+import mongoose from 'mongoose';
+
+const reviewSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    rating: {
+        type: Number,
+        required: true,
+    },
+    comment: {
+        type: String,
+        required: true,
+        trim: true
+    }
+}, { timestamps: true });
+
+const productSchema = mongoose.Schema({
+
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    brand: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    category: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    reviews: [reviewSchema],
+    avgRating: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    reviewCount: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    price: {
+        type: Number,
+        required: true,
+        default: 0,
+        trim: true
+    },
+    stockQuantity: {
+        type: Number,
+        required: true,
+        default: 0,
+        trim: true
+    },
+    price: {
+        type: Number,
+        required: true,
+        default: 0,
+        trim: true
+    }
+}, { timestamps: true });
+
+const Product = mongoose.model('Product', productSchema);
+
+export default Product;
