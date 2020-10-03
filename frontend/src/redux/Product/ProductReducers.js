263f8a5d4169b1743211bb576cbb8f5763f76296
badgersmans <s.hawnlaw6669@gmail.com>
2020-10-03 17:10:12 +0800

    c5/027 ProductList reducer and actions

---
+++ frontend/src/redux/Product/ProductReducers.js
@@ -0,0 +1,40 @@
+import {
+    PRODUCT_LIST_REQUEST,
+    PRODUCT_LIST_SUCCESS,
+    PRODUCT_LIST_FAIL
+} from './ProductTypes';
+
+const initialState = {
+    products: []
+};
+
+export const productListReducer = (state = initialState, action) => {
+
+    const { type, payload } = action;
+ 
+    switch (type) {
+        case PRODUCT_LIST_REQUEST:
+            return {
+                loading: true,
+                products: []
+            }
+        case PRODUCT_LIST_SUCCESS:
+            return {
+                loading: false,
+                products: payload
+            }
+        case PRODUCT_LIST_FAIL:
+            return {
+                loading: false,
+                error: payload
+            }
+        default:
+            return state;
+    }
+};
+
+
+
+
+
+
--- frontend/src/redux/RootReducer.js
+++ frontend/src/redux/RootReducer.js
@@ -1,11 +1,11 @@
 import { combineReducers } from 'redux';
-// import alertReducer from './alert/AlertReducer';
+import { productListReducer } from './Product/ProductReducers';
 // import authReducer from '../redux/auth/AuthReducer';
 // import profileReducer from '../redux/profile/ProfileReducer';
 // import postReducer from '../redux/post/PostReducer';
 
 export default combineReducers({
-    // alertReducer,
+    productList: productListReducer,
     // authReducer,
     // profileReducer,
     // postReducer
