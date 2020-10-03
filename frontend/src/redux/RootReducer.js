20cc5bcffb318c34e133cfe50e7e698878117d85
badgersmans <s.hawnlaw6669@gmail.com>
2020-10-03 16:46:05 +0800

    c5/026 redux setup

---
+++ frontend/src/redux/RootReducer.js
@@ -0,0 +1,12 @@
+import { combineReducers } from 'redux';
+// import alertReducer from './alert/AlertReducer';
+// import authReducer from '../redux/auth/AuthReducer';
+// import profileReducer from '../redux/profile/ProfileReducer';
+// import postReducer from '../redux/post/PostReducer';
+
+export default combineReducers({
+    // alertReducer,
+    // authReducer,
+    // profileReducer,
+    // postReducer
+});
