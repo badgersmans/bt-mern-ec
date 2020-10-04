443dacfa6a191a4c1c44397a538191074de3231e
badgersmans <s.hawnlaw6669@gmail.com>
2020-10-04 11:16:06 +0800

    c7/037 user auth endpoint

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorHandler.js';
 
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -20,6 +21,7 @@ app.use(express.json({ extended: false }));
 
 // define routes
 app.use('/api/products', productRoutes);
+app.use('/api/auth', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,22 +1,28 @@
 import mongoose from 'mongoose';
+import validator from 'validator';
+import bcrypt from 'bcryptjs';
+
 
 const userSchema = mongoose.Schema({
     name: {
         type: String,
-        required: true,
+        required: [true, 'Name is required'],
         lowercase: true,
         trim: true
     },
     email: {
         type: String,
-        required: true,
+        required: [true, 'Email is required'],
         unique: true,
         lowercase: true,
-        trim: true
+        trim: true,
+        validate: [validator.isEmail, 'Please provide a valid email']
     },
     password: {
         type: String,
-        required: true
+        required: [true, 'Password is required'],
+        minlength: 8,
+        select: false,
     },
     isAdmin: {
         type: Boolean,
@@ -24,6 +30,10 @@ const userSchema = mongoose.Schema({
         default: false
     }
 }, { timestamps: true });
+
+userSchema.methods.matchPassword = async function(inputPassword) {
+    return await bcrypt.compare(inputPassword, this.password)
+};
 
 const User = mongoose.model('User', userSchema);
 
