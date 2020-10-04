a82df11e832623eb90b95813abaa135dae50d48a
badgersmans <s.hawnlaw6669@gmail.com>
2020-10-04 15:34:07 +0800

    c7/042 user registration and password encryption

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -35,6 +35,18 @@ userSchema.methods.matchPassword = async function(inputPassword) {
     return await bcrypt.compare(inputPassword, this.password)
 };
 
+
+userSchema.pre('save', async function(next) {
+
+    if (!this.isModified('password')) {
+        next();
+    }
+
+    const salt    = await bcrypt.genSalt(13);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+
 const User = mongoose.model('User', userSchema);
 
 export default User;
