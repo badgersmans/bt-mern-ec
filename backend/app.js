f0ca1888a33c02225ba39414e72825f0843bc792
badgersmans <s.hawnlaw6669@gmail.com>
2020-10-08 13:09:54 +0800

    c12/077 added image upload

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express  from 'express';
 import dotenv   from 'dotenv';
 import colors   from 'colors';
@@ -7,6 +8,7 @@ import { notFound, errorHandler } from './middleware/errorHandler.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js';
 
 dotenv.config();
 
@@ -24,8 +26,13 @@ app.use(express.json({ extended: false }));
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
 
 app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID));
+
+// static folders
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 app.use(notFound);
 app.use(errorHandler);
