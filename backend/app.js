6df5ff66bd35a398bf906f52116063bfbca3b7ec
badgersmans <s.hawnlaw6669@gmail.com>
2020-10-09 15:49:08 +0800

    c14/087 prepare for deployment

--- backend/app.js
+++ backend/app.js
@@ -35,9 +35,18 @@ app.use('/api/upload', uploadRoutes);
 
 app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID));
 
+
 // static folders
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    });
+}
 
 app.use(notFound);
 app.use(errorHandler);
