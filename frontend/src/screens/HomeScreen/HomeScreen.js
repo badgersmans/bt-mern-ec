c1d3fa26f6eea48f7eb0beaf9ed8593ecf629b2f
badgersmans <s.hawnlaw6669@gmail.com>
2021-06-16 09:20:12 +0800

    changes to index.css and HomeScreen.js

--- frontend/src/screens/HomeScreen/HomeScreen.js
+++ frontend/src/screens/HomeScreen/HomeScreen.js
@@ -11,47 +11,61 @@ import Product from '../../components/Product/Product';
 import ProductCarousel from '../../components/ProductCarousel/ProductCarousel';
 
 const HomeScreen = ({ match }) => {
+  const searchText = match.params.searchText;
+  const pageNumber = match.params.pageNumber || 1;
 
-    const searchText = match.params.searchText;
-    const pageNumber = match.params.pageNumber || 1;
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  const { loading, error, products, page, pages } = useSelector(
+    (state) => state.productList
+  );
 
-    const { loading, error, products, page, pages } = useSelector(state => state.productList);
+  useEffect(() => {
+    dispatch(listProducts(searchText, pageNumber));
+  }, [dispatch, searchText, pageNumber]);
 
-    useEffect(() => {
-        dispatch(listProducts(searchText, pageNumber));
-    }, [dispatch, searchText, pageNumber]);
+  return (
+    <Fragment>
+      <Meta />
 
-    return (
+      {!searchText ? (
+        <ProductCarousel />
+      ) : (
+        <Link to='/' className='btn btn-dark'>
+          Go Back
+        </Link>
+      )}
+      <h1>Latest products</h1>
+
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
         <Fragment>
-            <Meta />
-                
-            {!searchText ? <ProductCarousel /> : <Link to='/' className='btn btn-dark'>Go Back</Link>}
-            <h1>Latest products</h1>
-
-            {
-                loading
-                ? <Loader />
-                : error ? <Message variant='danger'>{ error }</Message>
-
-                : (
-                    <Fragment>
-                        <Row>
-                            {
-                                products.map(product => (
-                                    <Col key={ product._id } sm={ 12 } md={ 6 } lg={ 4 } xl={ 3 }>
-                                        <Product product={ product }/>
-                                    </Col>
-                                ))
-                            }
-                        </Row>
-                        <Paginate pages={ pages } page={ page } searchText={ searchText ? searchText : ''}/>
-                    </Fragment>
-                )
-            }
+          <Row>
+            {products.map((product) => (
+              <Col
+                className='align-items-stretch d-flex'
+                key={product._id}
+                sm={12}
+                md={6}
+                lg={4}
+                xl={3}
+              >
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+          <Paginate
+            pages={pages}
+            page={page}
+            searchText={searchText ? searchText : ''}
+          />
         </Fragment>
-    )
+      )}
+    </Fragment>
+  );
 };
 
 export default HomeScreen;
