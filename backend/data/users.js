773f65bca2c74794416f3a0874e2a165d2a886de
badgersmans <s.hawnlaw6669@gmail.com>
2021-07-07 15:13:21 +0800

    changed seed data

--- backend/data/users.js
+++ backend/data/users.js
@@ -1,22 +1,22 @@
 import bcrypt from 'bcryptjs';
 
 const users = [
-    {
-      name: 'Admin user',
-      email: 'admin@geminids.com',
-      password: bcrypt.hashSync('12345678', 13),
-      isAdmin: true
-    },
-    {
-      name: 'Shawn',
-      email: 'shawnlaw6669@gmail.com',
-      password: bcrypt.hashSync('12345678', 13),
-    },
-    {
-      name: 'John DOE',
-      email: 'johndoe@gmail.com',
-      password: bcrypt.hashSync('12345678', 13),
-    },
+  {
+    name: 'Admin user',
+    email: 'admin@dominique.com',
+    password: bcrypt.hashSync('12345678', 13),
+    isAdmin: true,
+  },
+  {
+    name: 'Dominique_59',
+    email: 'dominique59@gmail.com',
+    password: bcrypt.hashSync('12345678', 13),
+  },
+  {
+    name: 'DiPierro',
+    email: 'test@gmail.com',
+    password: bcrypt.hashSync('12345678', 13),
+  },
 ];
 
 export default users;
