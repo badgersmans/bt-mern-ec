d3a898cd67f66ff713668f9d705a836b66107f3f
badgersmans <s.hawnlaw6669@gmail.com>
2021-07-10 19:11:02 +0800

    change seed script, and footer

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 
 const Footer = () => {
-    return (
-        <footer>
-            <Container>
-                <Row>
-                    <Col className="text-center py-3">
-                        Copyright &copy; Shawn
-                    </Col>
-                </Row>
-            </Container>
-        </footer>
-    )
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col className='text-center py-3'>
+            Copyright &copy; {new Date().getFullYear()} Geminids
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 };
 
 export default Footer;
