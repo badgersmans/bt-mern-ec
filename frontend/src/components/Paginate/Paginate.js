05493959897b882bd46f830535ea17177d54a78b
badgersmans <s.hawnlaw6669@gmail.com>
2021-07-10 17:34:41 +0800

    center pagination

--- frontend/src/components/Paginate/Paginate.js
+++ frontend/src/components/Paginate/Paginate.js
@@ -2,30 +2,27 @@ import React from 'react';
 import { Pagination } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
-const Paginate = ({ pages, page, isAdmin = false, searchText = ''}) => {
-    return pages > 1 && (
-        <Pagination>
-            {
-                [...Array(pages).keys()].map(x => (
-                    <LinkContainer
-                        key={ x + 1 }
-                        to={
-                            !isAdmin
-                            ? searchText
-                            ? `/search/${ searchText }/page/${ x + 1 }`
-                            : `/page/${ x + 1 }`
-
-                            : `/admin/productlist/${ x + 1 }`
-                        }
-                    >
-                        <Pagination.Item active={ x + 1 === page}>
-                            { x + 1 }
-                        </Pagination.Item>
-                    </LinkContainer>
-                ))
+const Paginate = ({ pages, page, isAdmin = false, searchText = '' }) => {
+  return (
+    pages > 1 && (
+      <Pagination className='d-flex justify-content-center'>
+        {[...Array(pages).keys()].map((x) => (
+          <LinkContainer
+            key={x + 1}
+            to={
+              !isAdmin
+                ? searchText
+                  ? `/search/${searchText}/page/${x + 1}`
+                  : `/page/${x + 1}`
+                : `/admin/productlist/${x + 1}`
             }
-        </Pagination>
+          >
+            <Pagination.Item active={x + 1 === page}>{x + 1}</Pagination.Item>
+          </LinkContainer>
+        ))}
+      </Pagination>
     )
+  );
 };
 
 export default Paginate;
